980ebba2dd1a2dbb8f85fce5750d882df0be3abb
João Pedro Gonçalves Viana de Souza <107888675+JoaoZalli@users.noreply.github.com>
2023-04-27 21:47:38 -0300

    Update index.h
    Mudei o nome do site também

--- Maozinha/MAOZINHA/index.h
+++ Maozinha/MAOZINHA/index.h
@@ -5,7 +5,7 @@ const char INDEX_HTML[] PROGMEM = R"=====(
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>AAAAAA</title>
+    <title>Mão Robótica - Protótipo</title>
     <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
     <style> 
         /*Estilo Site*/
